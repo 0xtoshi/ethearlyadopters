2f7e2624bf4cfde8a2a516f346dd51dee57c2560
0xToshi <jsonpayment@gmail.com>
2023-12-05 01:58:13 +0700

    Deploy

---
+++ pages/api/address.js
@@ -0,0 +1,43 @@
+import sqlite3 from "sqlite3";
+import { open, Database } from "sqlite";
+
+// Let's initialize it as null initially, and we will assign the actual database instance later.
+let db = null;
+
+export default async function handler(req, res) {
+  const address = req.query.eoa;
+  if (validateInputAddresses(address) == true) {
+    try {
+      if (!db) {
+        // If the database instance is not initialized, open the database connection
+        db = await open({
+          filename: "./eth.db", // Specify the database file path
+          driver: sqlite3.Database, // Specify the database driver (sqlite3 in this case)
+        });
+      }
+
+      const item = await db.get(
+        "SELECT * FROM early WHERE address = ?",
+        address.toLowerCase()
+      );
+
+      if (item !== undefined) {
+        res.status(200).json({ status: 200, data: item });
+      } else {
+        res.status(200).json({ status: 404, data: "Not Eligible" });
+      }
+
+      // => {name: "Albania", code: "AL"}
+    } catch (err) {
+      res.status(404).json({ status: 404, data: "Not Found" });
+      //throw { status: "error", message: "Error Fetching RPC" };
+    }
+  } else {
+    res.status(400).json({ status: 400, message: "Invalid Address" });
+    //throw { status: "error", message: "Invalid Ip Address" };
+  }
+}
+
+function validateInputAddresses(address) {
+  return /^(0x){1}[0-9a-fA-F]{40}$/i.test(address);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,165 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import axios from "axios";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from "react";
 
-export default function Home() {
+function validateInputAddresses(address) {
+  return /^(0x){1}[0-9a-fA-F]{40}$/i.test(address);
+}
+
+export default () => {
+  const [addressList, setAddressList] = useState();
+
+  const [result, setResult] = useState({
+    response: [],
+  });
+  const ChangeAddressList = (event) => {
+    setAddressList(event.target.value);
+  };
+
+  console.log(result.response);
+
+  const doCheck = async () => {
+    setResult((previousInputs) => ({
+      ...previousInputs,
+      response: [],
+    }));
+    try {
+      let address = addressList.split("\n");
+      if (address.length > 0) {
+        for (let i = 0; i < address.length; i++) {
+          if (validateInputAddresses(address[i])) {
+            try {
+              let data = await axios.get(`/api/address?eoa=${address[i]}`);
+              let result = data.data.data;
+              if (data.data.status === 200) {
+                setResult((previousInputs) => ({
+                  ...previousInputs,
+                  response: [
+                    ...previousInputs.response,
+                    {
+                      address: result.address,
+                      status: "Eligible",
+                      count: result.contract_count,
+                    },
+                  ],
+                }));
+              } else {
+                setResult((previousInputs) => ({
+                  ...previousInputs,
+                  response: [
+                    ...previousInputs.response,
+                    {
+                      address: address[i],
+                      status: "Not Eligible",
+                      count: 0,
+                    },
+                  ],
+                }));
+              }
+            } catch (err) {}
+          }
+        }
+      }
+    } catch (err) {}
+  };
+
+  const tableItems = result.response;
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Head>
+        <title>Mass Early Ethereum Adopters Checker | DMH</title>
+        <meta property="og:image" content="/checker.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
+      <section>
+        <div className="max-w-screen-xl mx-auto px-4 py-20 gap-12 text-gray-600 md:px-8">
+          <div className="space-y-5 max-w-4xl mx-auto text-center">
+            <h2 className="text-4xl text-gray-800 font-extrabold mx-auto md:text-5xl">
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4F46E5] to-[#E114E5]">
+                Ethereum Early Adopters Check | DMH
+              </span>
+            </h2>
+
+            <div className="items-center justify-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
+              <a
+                href="https://t.me/diary_mh"
+                target="__blank"
+                className="block py-2 px-4 text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none"
+              >
+                Join Tele Group
+              </a>
+              <a
+                href="https://raw.githubusercontent.com/FlipsiderEfer/Ethereum-Early-Adopters/main/Assets/Total.json"
+                target="__blank"
+                className="block py-2 px-4 text-white hover:text-gray-200 font-medium duration-150 active:bg-gray-300 border rounded-lg"
+              >
+                Source Repository
+              </a>
+            </div>
+          </div>
+          <div className="mt-14">
+            <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+              <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+                <label htmlFor="comment" className="sr-only"></label>
+                <textarea
+                  id="comment"
+                  rows={6}
+                  className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                  placeholder="EoA Address separated by new line"
+                  required=""
+                  defaultValue={addressList}
+                  onChange={ChangeAddressList}
+                />
+              </div>
+              <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+                <button
+                  onClick={doCheck}
+                  type="submit"
+                  className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+                >
+                  Check
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+        <div className="mt-12 relative h-max overflow-auto">
+          <table className="w-full table-auto text-sm text-left">
+            <thead className="text-white font-medium border-b">
+              <tr>
+                <th className="py-3 pr-6">Address</th>
+                <th className="py-3 pr-6">Status</th>
+                <th className="py-3 pr-6">Contract Interaction Count</th>
+              </tr>
+            </thead>
+            <tbody className="text-white divide-y">
+              {tableItems.map((item, idx) => (
+                <tr key={idx}>
+                  <td className="pr-6 py-4 whitespace-nowrap">
+                    {item.address}
+                  </td>
+                  <td className="pr-6 py-4 whitespace-nowrap">
+                    <span
+                      className={`px-3 py-2 rounded-full font-semibold text-xs ${
+                        item.status == "Eligible"
+                          ? "text-green-600 bg-green-50"
+                          : "text-red-600 bg-blue-50"
+                      }`}
+                    >
+                      {item.status}
+                    </span>
+                  </td>
+                  <td className="pr-6 py-4 whitespace-nowrap">{item.count}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+    </>
+  );
+};
