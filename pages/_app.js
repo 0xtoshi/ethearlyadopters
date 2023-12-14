ed396b94faa8e9950b767f8e92a6b564e656622c
0xToshi <jsonpayment@gmail.com>
2023-12-14 10:21:08 +0700

    Update GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { GoogleAnalytics } from "@eisberg-labs/next-google-analytics";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <GoogleAnalytics trackingId={"G-8YRY1JPB19"} />
       <Component {...pageProps} />
       <Analytics />
     </>
